f29c4e2454abab7a6bc7f3cd30f174713cc719c3
Juan G. Lievano <gabe@jglievano.me>
2017-01-10 09:55:32 -0800

    Improved fci-mode a bit.

--- jem/layers/+root/fill-column-indicator-boot.el
+++ jem/layers/+root/fill-column-indicator-boot.el
@@ -1,5 +1,17 @@
 ;; fill-column-indicator/boot.el
 
 (defun jem-root-fill-column-indicator|init ()
-  (require 'fill-column-indicator)
-  (setq fci-rule-column 80))
+  (use-package fill-column-indicator
+    :ensure t
+    :defer t
+    :init
+    (setq fci-rule-width 1)
+    (setq fci-rule-color "#D0BF8F")
+    (setq fci-rule-column 80)
+    (push '(fci-mode "") minor-mode-alist)
+    (jem-add-toggle fill-column-indicator
+      :status fci-mode
+      :on (turn-on-fci-mode)
+      :off (turn-off-fci-mode)
+      :documentation "Display the fill column indicator."
+      :evil-leader "tf")))
