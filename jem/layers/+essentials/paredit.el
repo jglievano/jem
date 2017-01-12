c257f248c785bef1f5433c72a929f910d68753ba
Gabriel Lievano <gabriell@google.com>
2017-01-12 11:57:33 -0800

    Adds paredit properly.
    Now works on linux!

--- jem/layers/+essentials/paredit.el
+++ jem/layers/+essentials/paredit.el
@@ -2,7 +2,7 @@
 
 (defun jem-essentials-paredit|init ()
   (use-package paredit
-    :load-path "paredit"
+    :load-path "third-party/paredit"
     :commands paredit-mode
     :diminish paredit-mode
     :config
